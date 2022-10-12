7eb95dea655aac504bd3025cc50bd3139553e27c
LuisanaHenmary <luisanahenmaryp@gmail.com>
2022-10-12 15:48:44 -0400

    Create the getCurrencyConvert function
    Update the file

--- pages/api/index.js
+++ pages/api/index.js
@@ -1,11 +1,31 @@
+import axios from "axios"
+const fixer_api = process.env.NEXT_PUBLIC_FIXER_KEY
+const curr_api = process.env.NEXT_PUBLIC_CURR_CONV_KEY
+
 export async function getSymbolsFixer() {
     try {
-        const responce = await fetch(`http://data.fixer.io/api/symbols?access_key=${process.env.FIXER_KEY}`)
-        const info = await responce.json()
-        const symbols = Object.keys(info.symbols)
+        const responce = await axios.get("http://data.fixer.io/api/symbols", { params: { access_key: fixer_api } })
+        const symbols = Object.keys(responce.data.symbols)
         return symbols
     }
     catch (error) {
         console.log(error)
+        return []
+    }
+}
+
+export async function getCurrencyConvert(query) {
+    try {
+        const resp = await axios.get("https://free.currconv.com/api/v7/convert", {
+            params: {
+                q: query,
+                compact: "ultra",
+                apiKey: curr_api
+            }
+        })
+        return resp.data[query]
+    }
+    catch (error) {
+        return -1
     }
 }
